cab97bf78246b529cff47d5837a59d612b96c0dd
tolgahan.ozturk <tolgahan.ozturk@saglik.gov.tr>
2022-06-26 23:29:37 +0300

    program duzenlendi.

--- Services/Catalog/Courses.Services.Catalog/Program.cs
+++ Services/Catalog/Courses.Services.Catalog/Program.cs
@@ -3,19 +3,19 @@ using Courses.Services.Catalog.Settings;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddScoped(typeof(ICategoryService), typeof(CategoryService));
-// Add services to the container.
+//typeof Program.cs dosyası diye belirttik bağlı bulunduğu project içindeki tüm automapper ları tarayacaktır.
 builder.Services.AddAutoMapper(typeof(Program));
+
+builder.Services.AddScoped(typeof(ICategoryService), typeof(CategoryService));
+
+var dbs = builder.Configuration.GetSection("DatabaseSettings").Get<DatabaseSettings>();
+builder.Services.AddSingleton<IDatabaseSettings, DatabaseSettings>(sp => { return dbs; });
+
+// Add services to the container.
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
-//typeof Program.cs dosyası diye belirttik bağlı bulunduğu project içindeki tüm automapper ları tarayacaktır.
-
-var dbs = builder.Configuration.GetSection("DatabaseSettings").Get<DatabaseSettings>();
-
-builder.Services.AddSingleton<IDatabaseSettings, DatabaseSettings>(sp => { return dbs; });
 
 var app = builder.Build();
 // Configure the HTTP request pipeline.
@@ -30,7 +30,3 @@ app.UseAuthorization();
 app.MapControllers();
 
 app.Run();
-
-//builder.Services.AddScoped(typeof(ICategoryService), typeof(CategoryService));
-
-//builder.Services.AddScoped(typeof(ICourseService), typeof(CourseService));
