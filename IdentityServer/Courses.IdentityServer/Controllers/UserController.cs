e6e15fcc694647ff9182f9fe44ffb639da8b8faa
tolgahan.ozturk <tolgahan.ozturk@saglik.gov.tr>
2022-07-13 21:51:55 +0300

    identityServer imp completed

---
+++ IdentityServer/Courses.IdentityServer/Controllers/UserController.cs
@@ -0,0 +1,47 @@
+﻿using Courses.IdentityServer.Dtos;
+using Courses.IdentityServer.Models;
+using Courses.Shared.Dtos;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+using System.Threading.Tasks;
+using static IdentityServer4.IdentityServerConstants;
+
+namespace Courses.IdentityServer.Controllers
+{
+    [Authorize(LocalApi.PolicyName)]
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public UserController(UserManager<ApplicationUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> SignUp(SignupDto signupDto)
+        {
+            //mapleme de yapılabilir di fakat tek bir alan için de
+            //automapper implement etmeye gerek yok bence
+            var user = new ApplicationUser
+            {
+                UserName = signupDto.UserName,
+                Email = signupDto.Email,
+                City = signupDto.City,
+            };
+
+            var result = await _userManager.CreateAsync(user, signupDto.Password);
+
+            if (!result.Succeeded)
+            {
+                return BadRequest(Response<NoContent>.Fail(result.Errors.Select(x => x.Description).ToList(), 404));
+            }
+
+            return NoContent();
+        }
+    }
+}
