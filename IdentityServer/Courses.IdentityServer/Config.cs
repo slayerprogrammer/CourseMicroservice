7ac71ae1d12a773dfde6d9e71e6844a3fe96138e
tolgahan.ozturk <tolgahan.ozturk@saglik.gov.tr>
2022-09-03 23:52:29 +0300

    fake payments completed

--- IdentityServer/Courses.IdentityServer/Config.cs
+++ IdentityServer/Courses.IdentityServer/Config.cs
@@ -16,6 +16,7 @@ namespace Courses.IdentityServer
             new ApiResource("resource_photo_stock"){Scopes={"photo_stock_fullpermission"}},
             new ApiResource("resource_basket"){Scopes={"basket_fullpermission"}},
             new ApiResource("resource_order"){Scopes={"order_fullpermission"}},
+            new ApiResource("resource_fake_payment"){Scopes={"fake_payment_fullpermission"}},
             new ApiResource("resource_discount"){Scopes={"discount_fullpermission"/*, "discount_readpermission", "discount_writepermission"*/}},
             new ApiResource(IdentityServerConstants.LocalApi.ScopeName)
         };
@@ -39,6 +40,7 @@ namespace Courses.IdentityServer
                 new ApiScope("basket_fullpermission", "Basket API için full erişim"),
                 new ApiScope("discount_fullpermission", "Discount API için full erişim"),
                 new ApiScope("order_fullpermission", "Order API için full erişim"),
+                new ApiScope("fake_payment_fullpermission", "Fake Payment API için full erişim"),
                 //new ApiScope("discount_readpermission", "Discount API için full erişim"),
                 //new ApiScope("discount_writepermission", "Discount API için full erişim"),
                 new ApiScope(IdentityServerConstants.LocalApi.ScopeName),
@@ -68,6 +70,7 @@ namespace Courses.IdentityServer
                     AllowedScopes = { "basket_fullpermission",
                                       "discount_fullpermission",
                                       "order_fullpermission",
+                                      "fake_payment_fullpermission",
                                       IdentityServerConstants.StandardScopes.Email,
                                       IdentityServerConstants.StandardScopes.OpenId,
                                       IdentityServerConstants.StandardScopes.Profile,
---
+++ Services/FakePayment/Courses.Services.FakePayment/Program.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc.Authorization;
+using System.IdentityModel.Tokens.Jwt;
+
+var builder = WebApplication.CreateBuilder(args);
+
+//policy yazalım.
+var requireAuthorizePolicy = new AuthorizationPolicyBuilder().RequireAuthenticatedUser().Build();
+
+//claimler gelirken sub identitynational olarak geliyor bize biz kişinin id sini alırken
+//sub diye aratıyoruz bundan dolayı map olayında sub ı devre dışı bıraktırdık
+JwtSecurityTokenHandler.DefaultInboundClaimTypeMap.Remove("sub");
+
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(options =>
+{
+    //tokenı kimin dağıttığı url bilgisini veriyoruz.
+    options.Authority = builder.Configuration.GetSection("IdentityServerUrl").Value;
+    options.Audience = "resource_fake_payment";
+    //https kullanmadığımız için alttakini ekliyoruz
+    options.RequireHttpsMetadata = false;
+});
+
+// Add services to the container.
+
+builder.Services.AddControllers(opt =>
+{
+    opt.Filters.Add(new AuthorizeFilter(requireAuthorizePolicy));
+});
+
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
